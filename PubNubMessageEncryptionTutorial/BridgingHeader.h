2f43a9cdae2e12dacbf8d7ba4976c7926e68a438
ertheis <ertheis@Erics-MacBook-Pro.local>
2014-07-09 12:22:56 -0700

    Setup Complete

---
+++ PubNubMessageEncryptionTutorial/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  PubNubMessageEncryptionTutorial
+//
+//  Created by Eric Theis on 7/9/14.
+//  Copyright (c) 2014 PubNub. All rights reserved.
+//
+
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import "PNImports.h"
+#endif
